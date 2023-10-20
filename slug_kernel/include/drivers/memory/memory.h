57df26ee61f9f4ff9f05944f8a0c270e4f0b777e
Paul Hariel <paul.hariel@outlook.com>
2023-10-20 21:47:23 +0200

    Add memory types

---
+++ slug_kernel/include/drivers/memory/memory.h
@@ -0,0 +1,34 @@
+#pragma once
+
+/*
+* This file defines types to represent memory in a convenient way.
+*/
+
+#include <stdint.h>
+
+namespace slug_kernel::drivers::memory
+{
+	struct physical_memory_block
+	{
+		bool free;
+
+		uint64_t size;
+
+		physical_memory_block* previous;
+		physical_memory_block* next;
+
+		physical_memory_block() = default;
+
+		// This function returns false if the block is not free.
+		bool merge_previous();
+
+		// This function returns false if the block is not free.
+		bool merge_next();
+
+		// This function returns false if the block is not free or if size is too big.
+		bool split(uint64_t const& size);
+
+		constexpr void* get_address() const;
+		constexpr uint64_t get_allocated_size() const;
+	};
+}
---
+++ slug_kernel/src/drivers/memory/memory.cpp
@@ -0,0 +1,81 @@
+#pragma once
+
+/*
+* This file implements types to represent memory in a convenient way.
+*/
+
+#include <stdint.h>
+
+#include "drivers/memory/memory.h"
+
+namespace slug_kernel::drivers::memory
+{
+	bool physical_memory_block::merge_previous()
+	{
+		if (!free)
+			return false;
+
+		if (!previous || !previous->free)
+			return true;
+
+		previous->size += size + sizeof(physical_memory_block);
+		previous->next = next;
+
+		if (next)
+			next->previous = previous;
+
+		return true;
+	}
+
+	bool physical_memory_block::merge_next()
+	{
+		if (!free)
+			return false;
+
+		if (!next || !next->free)
+			return true;
+
+		size += next->size + sizeof(physical_memory_block);
+		next = next->next;
+
+		if (next)
+			next->previous = this;
+
+		return true;
+	}
+
+	bool physical_memory_block::split(uint64_t const& size)
+	{
+		if (!free)
+			return false;
+
+		if (this->size < size + sizeof(physical_memory_block))
+			return false;
+
+		physical_memory_block* new_block = reinterpret_cast<physical_memory_block*>(reinterpret_cast<uint64_t>(this) + size + sizeof(physical_memory_block));
+
+		new_block->free = true;
+		new_block->size = this->size - size - sizeof(physical_memory_block);
+		new_block->previous = this;
+		new_block->next = next;
+
+		if (next)
+			next->previous = new_block;
+
+		this->size = size;
+		this->next = new_block;
+
+		return true;
+	}
+
+	constexpr void* physical_memory_block::get_address() const
+	{
+		return reinterpret_cast<void*>(reinterpret_cast<uint64_t>(this) + sizeof(physical_memory_block));
+	}
+
+	constexpr uint64_t physical_memory_block::get_allocated_size() const
+	{
+		return size - sizeof(physical_memory_block);
+	}
+
+}
